df7968944c9c4d78712998d8f0057459f889e341
Haku17113 <riku0925h@gmail.com>
2019-01-27 17:52:42 +0900

    upgrade: 次におけるマスを可視化
    next_boardsをstateで保持
    next_boardsは次におけるマスのx,y及びそのマスにおいた後の盤面next_boardを持つオブジェクト
    next_boardsを参照してrender()で次におけるマスに'I'をboardsに代入して表示

--- src/components/Game.js
+++ src/components/Game.js
@@ -5,33 +5,21 @@ import { Square } from './Square';
 
 const BOARD_SIZE = {x: 8, y:8};
 const PLAYER = {x: 'X', o: 'O'};
+const NEXT = 'I';
 
 export class Game extends React.Component {
 	constructor() {
 		super();
+		let board = initBoard();
+		let next_boards = getNextBoards(board, PLAYER.x);
 		this.state = {
 			history: [{
-				board: this.initBoard()
+				board: board,
+				next_boards: next_boards
 			}],
 			xIsNext: true,
 			stepNumber: 0
 		};
-	}
-
-	initBoard(){
-		let board = new Array(BOARD_SIZE.x);
-		for(let y = 0; y < BOARD_SIZE.y; y++) {
-			board[y] = new Array(BOARD_SIZE.y).fill(null);
-		}
-
-		board[BOARD_SIZE.x / 2][BOARD_SIZE.y / 2] = PLAYER.x;
-		board[BOARD_SIZE.x / 2][BOARD_SIZE.y / 2 - 1] = PLAYER.o;
-		board[BOARD_SIZE.x / 2 - 1][BOARD_SIZE.y / 2] = PLAYER.o;
-		board[BOARD_SIZE.x / 2 - 1][BOARD_SIZE.y / 2 - 1] = PLAYER.x;
-
-		console.log(board)
-
-		return board;
 	}
 	
 	handleClick(x, y) {
@@ -49,10 +37,12 @@ export class Game extends React.Component {
 		if (next_board == null){
 			return;
 		}
+		let next_boards = getNextBoards(next_board, !this.state.xIsNext ? PLAYER.x : PLAYER.o);
 
 		this.setState({
 			history: history.concat([{
-				board: next_board
+				board: next_board,
+				next_boards: next_boards
 			}]),
 			xIsNext: !this.state.xIsNext,
 			stepNumber: history.length
@@ -90,11 +80,16 @@ export class Game extends React.Component {
 		});
 		console.log(moves)
 
+		let board = _.cloneDeep(current.board);
+		current.next_boards.forEach((value) => {
+			board[value.x][value.y] = NEXT;
+		});
+
 		return (
 			<div className="game">
 				<div className="game-board">
 					<Board
-						board={current.board}
+						board={board}
 						onClick={(x, y) => this.handleClick(x, y)}
 						BOARD_SIZE = {BOARD_SIZE}
 					/>
@@ -108,12 +103,75 @@ export class Game extends React.Component {
 	}
 }
 
+function initBoard() {
+	let board = new Array(BOARD_SIZE.x);
+	for(let y = 0; y < BOARD_SIZE.y; y++) {
+		board[y] = new Array(BOARD_SIZE.y).fill(null);
+	}
+
+	board[BOARD_SIZE.x / 2][BOARD_SIZE.y / 2] = PLAYER.x;
+	board[BOARD_SIZE.x / 2][BOARD_SIZE.y / 2 - 1] = PLAYER.o;
+	board[BOARD_SIZE.x / 2 - 1][BOARD_SIZE.y / 2] = PLAYER.o;
+	board[BOARD_SIZE.x / 2 - 1][BOARD_SIZE.y / 2 - 1] = PLAYER.x;
+
+	console.log(board)
+
+	return board;
+}
+
 function calculateWinner(board) {
 
 	return null;
 }
 
+function getNextBoards(board, player){
+	let next_boards = [];
+
+	for(let x = 0; x < BOARD_SIZE.x; x++){
+		for(let y = 0; y < BOARD_SIZE.y; y++) {
+			let next_board = getNextBoard(board, x, y, player);
+			if(next_board){
+				next_boards = next_boards.concat({next_board: next_board, x: x, y: y});
+			}
+		}
+	}
+
+	console.log(next_boards)
+
+	return next_boards;
+}
+
 const ways={length: 8, x: [0, 0, 1, 1, 1, -1, -1, -1], y: [1, -1, 0, 1, -1, 0, 1, -1]};
+
+function getNextBoard(board, x, y, player) {
+	if(board[x][y] != null){
+		return;
+	}
+	
+	let next_board = _.cloneDeep(board);
+	next_board[x][y] = player;
+	let updated_squares_allways = [];
+	
+	for(let i = 0; i < ways.length; i++){
+		let updated_squares = getUpdatedSquares(next_board, x + ways.x[i], y + ways.y[i], player, i);
+		if(updated_squares){
+			updated_squares_allways = updated_squares_allways.concat(updated_squares);
+			// console.log(updated_squares)
+		}
+	}
+
+	if(updated_squares_allways.length == 0){
+		// console.log("updated_squares_allways is null")
+		next_board[x][y] = null;
+		return null;
+	}
+
+	updated_squares_allways.forEach((value) => {
+		next_board[value.x][value.y] = player;
+	});
+	
+	return next_board;
+}
 
 function getUpdatedSquares(board, x, y, player, way) {
 	if(x < 0 || y < 0 || x >= BOARD_SIZE.x || y >= BOARD_SIZE.y || board[x][y] == null){
@@ -130,29 +188,4 @@ function getUpdatedSquares(board, x, y, player, way) {
 			return null;
 		}
 	}
-}
-
-function getNextBoard(board, x, y, player) {
-	let next_board = _.cloneDeep(board);
-	next_board[x][y] = player;
-	let updated_squares_allways = [];
-	
-	for(let i = 0; i < ways.length; i++){
-		let updated_squares = getUpdatedSquares(next_board, x + ways.x[i], y + ways.y[i], player, i);
-		if(updated_squares){
-			updated_squares_allways = updated_squares_allways.concat(updated_squares);
-			console.log(updated_squares)
-		}
-	}
-
-	if(updated_squares_allways.length == 0){
-		console.log("updated_squares_allways is null")
-		return null;
-	}
-
-	updated_squares_allways.forEach((value) => {
-		next_board[value.x][value.y] = player;
-	});
-	
-	return next_board;
 }
